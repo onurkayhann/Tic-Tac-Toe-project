e172e96fce91c2a9bd4741e5961a25880cb19eaa
Onur Kayhan <onur.kayhan@chasacademy.se>
2024-09-07 19:37:39 +0200

    add checkWinner function to determine the winner

--- TicTacToe/Model/TicTacToe.swift
+++ TicTacToe/Model/TicTacToe.swift
@@ -19,7 +19,6 @@ class TicTacToe {
     // Player state
     var currentPlayer = true
     
-    
     func startGame(at index: Int) {
         
         if gameArray[index] == 0 {
@@ -33,6 +32,89 @@ class TicTacToe {
             
             currentPlayer.toggle()
             
+        }
+        
+        checkWinner()
+        
+    }
+    
+    func checkWinner() {
+        
+        // Possibilites for player one to win horizontally:
+        if (gameArray[0] == PLAYER_ONE && gameArray[1] == PLAYER_ONE && gameArray[2] == PLAYER_ONE) {
+            print("Player one wins by horizontal combination in horizontal row 1")
+        }
+        
+        if (gameArray[3] == PLAYER_ONE && gameArray[4] == PLAYER_ONE && gameArray[5] == PLAYER_ONE) {
+            print("Player one wins by horizontal combination in horizontal row 2")
+        }
+        
+        if (gameArray[6] == PLAYER_ONE && gameArray[7] == PLAYER_ONE && gameArray[8] == PLAYER_ONE) {
+            print("Player one wins by horizontal combination in horizontal row 3")
+        }
+        
+        // Possibilites for player one to win vertically:
+        if (gameArray[0] == PLAYER_ONE && gameArray[3] == PLAYER_ONE && gameArray[6] == PLAYER_ONE) {
+            print("Player one wins by vertical combination in row 1")
+        }
+        
+        if (gameArray[1] == PLAYER_ONE && gameArray[4] == PLAYER_ONE && gameArray[7] == PLAYER_ONE) {
+            print("Player one wins by vertical combination in row 2")
+        }
+        
+        if (gameArray[2] == PLAYER_ONE && gameArray[5] == PLAYER_ONE && gameArray[8] == PLAYER_ONE) {
+            print("Player one wins by vertical combination in row 3")
+        }
+        
+        // Possibilites for player one to win dioganally:
+        
+        if (gameArray[0] == PLAYER_ONE && gameArray[4] == PLAYER_ONE && gameArray[8] == PLAYER_ONE) {
+            print("Player one wins by dioganal combination")
+        }
+        
+        if (gameArray[2] == PLAYER_ONE && gameArray[4] == PLAYER_ONE && gameArray[6] == PLAYER_ONE) {
+            print("Player one wins by dioganal combination")
+        }
+        
+        
+        // Possibilites for player two to win horizontally:
+        if (gameArray[0] == PLAYER_TWO && gameArray[1] == PLAYER_TWO && gameArray[2] == PLAYER_TWO) {
+            print("Player two wins by horizontal combination in horizontal row 1")
+        }
+        
+        if (gameArray[3] == PLAYER_TWO && gameArray[4] == PLAYER_TWO && gameArray[5] == PLAYER_TWO) {
+            print("Player two wins by horizontal combination in horizontal row 2")
+        }
+        
+        if (gameArray[6] == PLAYER_TWO && gameArray[7] == PLAYER_TWO && gameArray[8] == PLAYER_TWO) {
+            print("Player two wins by horizontal combination in horizontal row 3")
+        }
+        
+        // Possibilites for player two to win vertically:
+        if (gameArray[0] == PLAYER_TWO && gameArray[3] == PLAYER_TWO && gameArray[6] == PLAYER_TWO) {
+            print("Player two wins by vertical combination in row 1")
+        }
+        
+        if (gameArray[1] == PLAYER_TWO && gameArray[4] == PLAYER_TWO && gameArray[7] == PLAYER_TWO) {
+            print("Player two wins by vertical combination in row 2")
+        }
+        
+        if (gameArray[2] == PLAYER_TWO && gameArray[5] == PLAYER_TWO && gameArray[8] == PLAYER_TWO) {
+            print("Player two wins by vertical combination in row 3")
+        }
+        
+        // Possibilites for player two to win dioganally:
+        
+        if (gameArray[0] == PLAYER_TWO && gameArray[4] == PLAYER_TWO && gameArray[8] == PLAYER_TWO) {
+            print("Player two wins by dioganal combination")
+        }
+        
+        if (gameArray[2] == PLAYER_TWO && gameArray[4] == PLAYER_TWO && gameArray[6] == PLAYER_TWO) {
+            print("Player two wins by dioganal combination")
+        }
+        
+        if (!gameArray.contains(0)) {
+            print("It's a tie!")
         }
         
     }
